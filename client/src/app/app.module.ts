10ecab700e87891533c9223e587744166fc0d646
Oblivious <me262thanos@gmail.com>
2019-08-14 19:23:17 +0300

    Font awesome and Bulma setup

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFontAwesomeModule } from "angular-font-awesome";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
