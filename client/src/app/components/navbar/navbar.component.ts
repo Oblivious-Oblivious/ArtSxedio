f2137b8080578711f89f9256884a3eb037a05958
Oblivious <me262thanos@gmail.com>
2019-08-19 02:31:05 +0300

    Fixed footer margins and fonts
    Created variables for colors

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -8,15 +8,31 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 export class NavbarComponent implements OnInit {
   @ViewChild("navBurger", { static: false }) navBurger: ElementRef;
   @ViewChild("navMenu", { static: false }) navMenu: ElementRef;
+  @ViewChild("darkThemeToggle", { static: false }) darkThemeToggle: ElementRef;
 
-  constructor() { }
+  constructor() {
+    // const toggleSwitch = document.querySelector('.dark-theme-toggle');
+
+    // function switchTheme(e) {
+    //   if (e.target.checked) {
+    //     document.documentElement.setAttribute('data-theme', 'dark');
+    //   }
+    //   else {
+    //     document.documentElement.setAttribute('data-theme', 'light');
+    //   }
+    // }
+  }
 
   ngOnInit() {
   }
-  
+
   toggleNavbar() {
     this.navBurger.nativeElement.classList.toggle("is-active");
     this.navMenu.nativeElement.classList.toggle("is-active");
     this.navMenu.nativeElement.style.setProperty("--mobile", true);
+  }
+
+  switchTheme() {
+    this.darkThemeToggle.nativeElement.classList.toggle("is-outlined");
   }
 }
