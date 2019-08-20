5f0cbf68adaccb69222a8bf3c0525cfbbf267455
Oblivious <me262thanos@gmail.com>
2019-08-21 02:49:58 +0300

    Added a product card to the shop page

--- client/src/app/components/shop/shop.component.ts
+++ client/src/app/components/shop/shop.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.scss']
+  styleUrls: ['./shop.component.scss'],
 })
 export class ShopComponent implements OnInit {
+  images = ["image1", "image2", "image3", "image4"];
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
