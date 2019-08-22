3b3f185a07c7f51a60e36e65bc1f7551b863a4b1
Oblivious <me262thanos@gmail.com>
2019-08-22 23:22:26 +0300

    Put some css fields into variables
    Improved dark mode

--- client/src/app/models/theme.model.ts
+++ client/src/app/models/theme.model.ts
@@ -6,19 +6,23 @@ export interface Theme {
 export const light: Theme = {
     name: "light",
     properties: {
-        "--bg-color": "''",
-        "--text-color": "#181818",
+        "--dark-bg": "''",
+        "--body-bg-color": "#f0f0f0",
+        "--text-color": "#4e4e4e",
         "--anchor-color": "#ffffff",
-        "--border-color": "#181818"
+        "--border-color": "#181818",
+        "--shadow-color": "rgba(24, 24, 24, 0.5)"
     }
 };
 
 export const dark: Theme = {
     name: "dark",
     properties: {
-        "--bg-color": "linear-gradient(to right bottom, #181818, #191919, #1b191a, #1d1a1a, #1e1b1a, #322720, #433624, #4f4729, #776a33, #a28e3d, #cfb446, #ffdc4f)",
+        "--dark-bg": "linear-gradient(to right bottom, #181818, #191919, #1b191a, #1d1a1a, #1e1b1a, #322720, #433624, #4f4729, #776a33, #a28e3d, #cfb446, #ffdc4f)",
+        "--body-bg-color": "#363636",
         "--text-color": "#ffffff",
         "--anchor-color": "#181818",
-        "--border-color": "#ffffff"
+        "--border-color": "#ffffff",
+        "--shadow-color": "#ffffffbb"
     }
 };
