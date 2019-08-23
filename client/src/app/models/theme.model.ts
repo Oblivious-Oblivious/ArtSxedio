37c842ff780b13cc7f95384a918bcd331de74e34
Oblivious <me262thanos@gmail.com>
2019-08-23 19:52:55 +0300

    Finalized dark mode

--- client/src/app/models/theme.model.ts
+++ client/src/app/models/theme.model.ts
@@ -18,6 +18,10 @@ export const light: Theme = {
 export const dark: Theme = {
     name: "dark",
     properties: {
+        // "--dark-bg": "linear-gradient(to right, #e55d87, #5fc3e4)",
+        // "--dark-bg": "linear-gradient(315deg, #ffa69e 0%, #5d4954 74%)",
+        // "--dark-bg": "linear-gradient(315deg, #756213 0%, #181818 74%)",
+        // "--dark-bg": "linear-gradient(315deg, #ffeaa7 0%, #181818 74%)",
         "--dark-bg": "linear-gradient(to right bottom, #181818, #191919, #1b191a, #1d1a1a, #1e1b1a, #322720, #433624, #4f4729, #776a33, #a28e3d, #cfb446, #ffdc4f)",
         "--body-bg-color": "#363636",
         "--text-color": "#ffffff",
