bded66faf899ad71789ded575ca24f86a50afb72
Oblivious <me262thanos@gmail.com>
2019-11-01 13:58:05 +0200

    .

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,57 @@
+/**
+
+## Full Stack Tools
+
+# Front End
+1) HTML
+2) Bulma
+3) Angular
+
+# Back End
+4) Express
+
+# Database
+5) MongoDB (NoSQL)
+6) Postgres (SQL)
+2
+# Desktop
+7) Electron
+
+# Mobile
+8) Ionic
+9) Meteor
+10) React-Native
+
+# Machine Learning
+11) Tensorflow
+
+# Unit Testing
+12) Jasmine Framework, Karma Task Runner **(Angular)** **(Ionic)**
+13) Mocha Testing Environment, Chai Assertion Library **(Express)** **(Meteor)**
+14) Spectron Framework **(Electron)**
+15) Jest Framework, Enzyme **(React-Native)** **(Tensorflow)**
+
+# Version Control
+16) GitHub
+
+# Continuous Integration
+17) Jetkins
+18) Circle CI
+19) Travis CI
+
+# Deployment System
+20) Heroku
+
+# Optional Tools
+**I)** Sass
+**II)** Typescript
+**III)** Gulp
+**IV)** Webpack
+**V)** Docker
+
+
+ */
+
 /* Dependencies */
 const gulp = require("gulp");
 const clean = require("gulp-clean");
