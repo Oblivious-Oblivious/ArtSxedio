818d3cda9617650aa20e1c117e8fa1627b0a548e
Oblivious <me262thanos@gmail.com>
2021-08-25 01:08:34 +0300

    Fixed paths for database.

--- mongodb/insert_to_mongo.js
+++ mongodb/insert_to_mongo.js
@@ -12,8 +12,8 @@ mongoose.connect(db, {
 const imageList = [
     /* Tall */
     new Image({
-        src: "./assets/img/tall/Boat in the Sunset.jpg",
-        details: ["./assets/img/tall/details/Boat in the Sunset detail.jpg"],
+        src: "./assets/tall/Boat in the Sunset.jpg",
+        details: ["./assets/tall/details/Boat in the Sunset detail.jpg"],
         title: "Boat in the Sunset",
         size: "tall",
         price: 80,
@@ -21,8 +21,8 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/tall/Dali.jpg",
-        details: ["./assets/img/tall/details/Dali detail.jpg"],
+        src: "./assets/tall/Dali.jpg",
+        details: ["./assets/tall/details/Dali detail.jpg"],
         size: "tall",
         title: "Dali",
         price: 100,
@@ -30,7 +30,7 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/tall/Lighthouse.jpg",
+        src: "./assets/tall/Lighthouse.jpg",
         details: [],
         size: "tall",
         title: "Lighthouse",
@@ -39,8 +39,8 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/tall/Old House.jpg",
-        details: ["./assets/img/tall/details/Old House detail.jpg"],
+        src: "./assets/tall/Old House.jpg",
+        details: ["./assets/tall/details/Old House detail.jpg"],
         size: "tall",
         title: "Old House",
         price: 80,
@@ -48,8 +48,8 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/tall/Pencil.jpg",
-        details: ["./assets/img/tall/details/Pencil detail.jpg"],
+        src: "./assets/tall/Pencil.jpg",
+        details: ["./assets/tall/details/Pencil detail.jpg"],
         size: "tall",
         title: "Pencil",
         price: 80,
@@ -57,7 +57,7 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/tall/Poppies.jpg",
+        src: "./assets/tall/Poppies.jpg",
         details: [],
         size: "tall",
         title: "Poppies",
@@ -66,7 +66,7 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/tall/River.jpg",
+        src: "./assets/tall/River.jpg",
         details: [],
         size: "tall",
         title: "River",
@@ -75,8 +75,8 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/tall/Rose.jpg",
-        details: ["./assets/img/tall/details/Rose detail.jpg"],
+        src: "./assets/tall/Rose.jpg",
+        details: ["./assets/tall/details/Rose detail.jpg"],
         size: "tall",
         title: "Rose",
         price: 100,
@@ -84,8 +84,8 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/tall/Salt Shaker.jpg",
-        details: ["./assets/img/tall/details/Salt Shaker detail.jpg"],
+        src: "./assets/tall/Salt Shaker.jpg",
+        details: ["./assets/tall/details/Salt Shaker detail.jpg"],
         size: "tall",
         title: "Salt Shaker",
         price: 100,
@@ -93,7 +93,7 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/tall/Sunny Snow.jpg",
+        src: "./assets/tall/Sunny Snow.jpg",
         details: [],
         size: "tall",
         title: "Sunny Snow",
@@ -102,7 +102,7 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/tall/Ver Meer.jpg",
+        src: "./assets/tall/Ver Meer.jpg",
         details: [],
         size: "tall",
         title: "Ver Meer",
@@ -113,8 +113,8 @@ const imageList = [
 
     /* Wide */
     new Image({
-        src: "./assets/img/wide/Figs.jpg",
-        details: ["./assets/img/wide/details/Figs detail.jpg"],
+        src: "./assets/wide/Figs.jpg",
+        details: ["./assets/wide/details/Figs detail.jpg"],
         size: "wide",
         title: "Figs",
         price: 100,
@@ -122,10 +122,10 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/wide/Fruit.jpg",
+        src: "./assets/wide/Fruit.jpg",
         details: [
-            "./assets/img/wide/details/Fruit detail1.jpg",
-            "./assets/img/wide/details/Fruit detail2.jpg",
+            "./assets/wide/details/Fruit detail1.jpg",
+            "./assets/wide/details/Fruit detail2.jpg",
         ],
         size: "wide",
         title: "Fruit",
@@ -134,11 +134,11 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/wide/Leaves.jpg",
+        src: "./assets/wide/Leaves.jpg",
         details: [
-            "./assets/img/wide/details/Leaves detail1.jpg",
-            "./assets/img/wide/details/Leaves detail2.jpg",
-            "./assets/img/wide/details/Leaves detail3.jpg",
+            "./assets/wide/details/Leaves detail1.jpg",
+            "./assets/wide/details/Leaves detail2.jpg",
+            "./assets/wide/details/Leaves detail3.jpg",
         ],
         size: "wide",
         title: "Leaves",
@@ -147,7 +147,7 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/wide/Night Waves.jpg",
+        src: "./assets/wide/Night Waves.jpg",
         details: [],
         size: "wide",
         title: "Night Waves",
@@ -156,7 +156,7 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/wide/Peaches.jpg",
+        src: "./assets/wide/Peaches.jpg",
         details: [],
         size: "wide",
         title: "Peaches",
@@ -165,8 +165,8 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/wide/River Boat.jpg",
-        details: ["./assets/img/wide/details/River Boat detail.jpg"],
+        src: "./assets/wide/River Boat.jpg",
+        details: ["./assets/wide/details/River Boat detail.jpg"],
         size: "wide",
         title: "River Boat",
         price: 100,
@@ -174,7 +174,7 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/wide/Rocks.jpg",
+        src: "./assets/wide/Rocks.jpg",
         details: [],
         size: "wide",
         title: "Rocks",
@@ -183,10 +183,10 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/wide/Snowy Village.jpg",
+        src: "./assets/wide/Snowy Village.jpg",
         details: [
-            "./assets/img/wide/details/Snowy Village detail1.jpg",
-            "./assets/img/wide/details/Snowy Village detail2.jpg",
+            "./assets/wide/details/Snowy Village detail1.jpg",
+            "./assets/wide/details/Snowy Village detail2.jpg",
         ],
         size: "wide",
         title: "Snowy Village",
@@ -195,7 +195,7 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/wide/Vase.jpg",
+        src: "./assets/wide/Vase.jpg",
         details: [],
         size: "wide",
         title: "Vase",
@@ -204,7 +204,7 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/wide/Waves.jpg",
+        src: "./assets/wide/Waves.jpg",
         details: [],
         size: "wide",
         title: "Waves",
@@ -215,7 +215,7 @@ const imageList = [
 
     /* Ultra Wide */
     new Image({
-        src: "./assets/img/ultra-wide/Cactus.jpg",
+        src: "./assets/ultra-wide/Cactus.jpg",
         details: [],
         size: "ultra-wide",
         title: "Cactus",
@@ -224,7 +224,7 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/ultra-wide/Naxos.jpg",
+        src: "./assets/ultra-wide/Naxos.jpg",
         details: [],
         size: "ultra-wide",
         title: "Naxos",
@@ -233,10 +233,10 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/ultra-wide/Pumkin.jpg",
+        src: "./assets/ultra-wide/Pumkin.jpg",
         details: [
-            "./assets/img/ultra-wide/details/Pumkin detail1.jpg",
-            "./assets/img/ultra-wide/details/Pumkin detail2.jpg",
+            "./assets/ultra-wide/details/Pumkin detail1.jpg",
+            "./assets/ultra-wide/details/Pumkin detail2.jpg",
         ],
         size: "ultra-wide",
         title: "Pumkin",
@@ -245,10 +245,10 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/ultra-wide/Quay.jpg",
+        src: "./assets/ultra-wide/Quay.jpg",
         details: [
-            "./assets/img/ultra-wide/details/Quay detail1.jpg",
-            "./assets/img/ultra-wide/details/Quay detail2.jpg",
+            "./assets/ultra-wide/details/Quay detail1.jpg",
+            "./assets/ultra-wide/details/Quay detail2.jpg",
         ],
         size: "ultra-wide",
         title: "Quay",
@@ -257,7 +257,7 @@ const imageList = [
         type: "Acrilic",
     }),
     new Image({
-        src: "./assets/img/ultra-wide/Quince.jpg",
+        src: "./assets/ultra-wide/Quince.jpg",
         size: "ultra-wide",
         details: [],
         title: "Quince",
